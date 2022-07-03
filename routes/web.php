bff731d165bf4a04ce82f9e87cf3aad0b7c1b2b4
Takaaki-Shimizu <tattaka_feelmysoul@yahoo.co.jp>
2022-07-03 09:00:31 +0900

    MVC記述の練習

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
 });
+
+Route::get('tests/test', 'App\Http\Controllers\TestController@index');
